2c3994afe00a08ac9edd76b46643c1536666d94d
Moghany <m.osama.elmoghany@gmail.com>
2024-03-13 15:17:57 +0200

    adding api folder init

---
+++ watchlist_app/api/serializers.py
@@ -0,0 +1,8 @@
+#we will map each field in serializers instead of using the model class
+from rest_framework import serializers
+
+class MovieSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField()
+    description = serializers.CharField()
+    active = serializers.BooleanField()
