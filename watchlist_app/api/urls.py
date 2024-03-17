697799bc085f31d6b86375278a3239e3a04474c1
Moghany <m.osama.elmoghany@gmail.com>
2024-03-17 13:02:12 +0200

    ModelSerializer + WatchList + StreamPlatform

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -6,10 +6,12 @@ from django.urls import path
 #     path('<int:pk>/', movie_detail, name='movie-detail-page'),
 # ]
 
-from watchlist_app.api.views import WatchDetailAV, WatchListAV, StreamPlatformAV, StreamDetailAV
+from watchlist_app.api.views import WatchDetailAV, WatchListAV, StreamPlatformAV, StreamDetailAV, ReviewList, ReviewDetail
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='watch-list-page'),
     path('<int:pk>/', WatchDetailAV.as_view(), name='watch-detail-page'),
     path('stream/', StreamPlatformAV.as_view(), name='streamplatform-page'),
     path('stream/<int:pk>/', StreamDetailAV.as_view(), name='streamplatform-detail'),
+    path('review/', ReviewList.as_view(), name='review-page'),
+    path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail-page'),
 ]
