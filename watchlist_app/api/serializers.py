bbdd95e96c1b112626288a56fa1c2e07881cf654
Moghany <m.osama.elmoghany@gmail.com>
2024-03-17 07:50:54 +0200

    ModelSerializer + WatchList + StreamPlatform

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -2,11 +2,6 @@
 from rest_framework import serializers
 from watchlist_app.models import WatchList, StreamPlatform
 
-class StreamPlatformSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = StreamPlatform
-        fields = "__all__"
-    
 class WatchListSerializer(serializers.ModelSerializer):
     #crete, update, delete is automatically defined using ModelSerializer!
     # len_name = serializers.SerializerMethodField()
@@ -17,6 +12,14 @@ class WatchListSerializer(serializers.ModelSerializer):
         fields = "__all__"
         # fields = ['id', 'name', 'description']
         # exclude = ['active']
+
+class StreamPlatformSerializer(serializers.ModelSerializer):
+    platform_watchlist = WatchListSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = StreamPlatform
+        fields = "__all__"
+    
 
     # def get_len_name(self, object):
     #     return len(object.name)
