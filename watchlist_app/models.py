eb44a857c983bea584f3571d4c05f3e83c53ad14
Moghany <m.osama.elmoghany@gmail.com>
2024-03-17 12:14:56 +0200

    ModelSerializer + WatchList + StreamPlatform

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator 
 
 # Create your models here.
 class StreamPlatform(models.Model):
@@ -19,3 +20,14 @@ class WatchList(models.Model):
     def __str__(self):
         return self.title
     
+#1 movie => multiple reviews
+class Review(models.Model):
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    description = models.CharField(max_length=200, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    watchlist = models.ForeignKey(WatchList, on_delete=models.CASCADE, related_name="reviews_watchlist")
+    
+    def __str__(self):
+        return self.rating
