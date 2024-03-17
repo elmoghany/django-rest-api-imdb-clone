adffe5642519fdddd4c42ceeedef0d7830a33c81
Moghany <m.osama.elmoghany@gmail.com>
2024-03-17 12:17:48 +0200

    ModelSerializer + WatchList + StreamPlatform

--- watchlist_app/admin.py
+++ watchlist_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 # from .models import WatchList
-from watchlist_app.models import WatchList, StreamPlatform
+from watchlist_app.models import WatchList, StreamPlatform, Review
 
 # Register your models here.
 admin.site.register(WatchList)
 admin.site.register(StreamPlatform)
+admin.site.register(Review)
