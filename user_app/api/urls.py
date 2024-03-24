336c600383f81eabb29909a403ed923b28aeea74
Moghany <m.osama.elmoghany@gmail.com>
2024-03-24 06:29:12 +0200

    adding auth token + registration

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from user_app.api.views import registration_view
+from user_app.api.views import registration_view, logout_view
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login-page'),
     path('register/', registration_view, name='register-page'),
+    path('logout/', logout_view, name='logout-page'),
 ]
