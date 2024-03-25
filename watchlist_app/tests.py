adf123533f068dfb72806996c045cbea15de6a40
Moghany <m.osama.elmoghany@gmail.com>
2024-03-25 17:05:53 +0200

    adding auto review testing

--- watchlist_app/tests.py
+++ watchlist_app/tests.py
@@ -69,18 +69,64 @@ class ReviewTestCase(test.APITestCase):
     def setUp(self):
         self.user = User.objects.create_user(username="example", password="test@123")
         self.token = Token.objects.get(user__username=self.user)
-        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
-        self.stream = models.StreamPlatform.objects.create(name="netflix", about="# sds sd", website="https://wnndnd.com")
-        self.watchlist = models.WatchList.objects.create(platform=self.stream, title="example movie", description="description storyline", active=True)
-
+        self.stream = models.StreamPlatform.objects.create(name="netflix", 
+                                                           about="# sds sd", 
+                                                           website="https://www.wnndnd.com")
+        self.watchlist = models.WatchList.objects.create(platform=self.stream, 
+                                                         title="example movie", 
+                                                         description="description storyline", 
+                                                         active=True)
+        self.watchlist2 = models.WatchList.objects.create(platform=self.stream, 
+                                                         title="example movie", 
+                                                         description="description storyline", 
+                                                         active=True)
+        self.review = models.Review.objects.create(review_username=self.user, rating=5, description="bla bla", watchlist=self.watchlist2, active=True)
+        
     def test_review_create(self):
         data = {
-        "review_user" : self.user,
-        "rating": 5,
-        "description": "great movie",
-        "watchlist": self.watchlist,
-        "active": True
+            "review_username" : self.user,
+            "rating": 5,
+            "description": "great movie",
+            "watchlist": self.watchlist,
+            "active": True
         }
         response = self.client.post(reverse('review-create-page', args=(self.watchlist.id,)), data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        response = self.client.post(reverse('review-create-page', args=(self.watchlist.id,)), data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_review_create_unauth(self):
+        data = {
+            "review_username" : self.user,
+            "rating": 5,
+            "description": "great movie",
+            "watchlist": self.watchlist,
+            "active": True
+        }
+        self.client.force_authenticate(user=None)
+        response = self.client.post(reverse('review-create-page', args=(self.watchlist.id,)), data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_review_update(self):
+        data = {
+            "review_username" : self.user,
+            "rating": 4,
+            "description": "great movie - updated",
+            "watchlist": self.watchlist,
+            "active": False
+        }
+        response = self.client.put(reverse('review-detail-page', args=(self.review.id,)), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_review_list(self):
+        response = self.client.get(reverse('review-list-page', args=(self.watchlist.id,)))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_review_individual(self):
+        response = self.client.get(reverse('review-detail-page', args=(self.review.id,)))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    
