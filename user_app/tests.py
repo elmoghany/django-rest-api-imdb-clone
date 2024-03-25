590f47efffed80aee76315eca11b06d0fceae622
Moghany <m.osama.elmoghany@gmail.com>
2024-03-25 14:24:34 +0200

    adding auto login logout testing

--- user_app/tests.py
+++ user_app/tests.py
@@ -17,3 +17,22 @@ class RegisterTestCase(test.APITestCase):
         }
         response = self.client.post(reverse('register-page'),data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+class LoginLogoutTestCase(test.APITestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(username="example", password="test@123")
+        
+    def test_login(self):
+        data = {
+            "username": "example",
+            "password": "test@123"
+        }
+        response = self.client.post(reverse('login-page'), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_logout(self):
+        self.token = Token.objects.get(username__username="example")
+        self.client.credentials(HTTP_AUTHORIZATION="Token" + token.key)
+        response = self.client.post(reverse('logout-page'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
