c6c8f282c683563efd458bb8de52bacc8905c46e
Moghany <m.osama.elmoghany@gmail.com>
2024-03-25 10:40:04 +0200

    adding pagination

--- watchlist_app/api/pagination.py
+++ watchlist_app/api/pagination.py
@@ -3,3 +3,5 @@ from rest_framework import pagination
 class WatchListPagination(pagination.PageNumberPagination):
     page_size = 10
     page_query_param = 'page_number'
+    page_size_query_param = 'size'
+    max_page_size = 10
