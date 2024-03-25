7c3716c6865eff38a01f2b64fdd66a2d32bca30f
Moghany <m.osama.elmoghany@gmail.com>
2024-03-25 13:39:09 +0200

    adding auto registration testing

--- user_app/tests.py
+++ user_app/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+# from django.test import TestCase
+from rest_framework import test
+from rest_framework import status
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+class RegisterTestCase(test.APITestCase):
+    
+    def test_register(self):
+        data = {
+            "username": "testcase",
+            "email": "test1@example.com",
+            "password": "test@123",
+            "password2": "test@123",
+        }
+        response = self.client.post(reverse('register-page'),data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
