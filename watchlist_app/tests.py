5a487db324f33b8a4b6db00df68fe778ce38c5b0
Moghany <m.osama.elmoghany@gmail.com>
2024-03-26 11:51:45 +0200

    adding auto review testing

--- watchlist_app/tests.py
+++ watchlist_app/tests.py
@@ -129,4 +129,7 @@ class ReviewTestCase(test.APITestCase):
         response = self.client.get(reverse('review-detail-page', args=(self.review.id,)))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         
-    
+    def test_review_user(self):
+        response = self.client.get('/watch/reviews/?username' + self.user.username)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
